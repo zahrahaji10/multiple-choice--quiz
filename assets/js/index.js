07042422a2f74d9dd20df870a29ea2693141afc9
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-04-28 15:20:57 +0100

    inputed sections , styted css and started js

---
+++ assets/js/index.js
@@ -0,0 +1,79 @@
+// declare the timer variable here
+
+const questions = [
+  //question 1
+  {
+    questionTitle: "What does HTML stand for in coding?",
+    answers: [
+      "Healthcare Technology management Layout",
+      "Hey That’s My Lunch",
+      "Hypertext Markup language",
+      "How To Marinate Lemons",
+    ],
+    correctAnswer: "Hypertext Markup language",
+  },
+  //question 2
+  {
+    questionTitle: "What is the correct CSS syntax",
+    answers: [
+      "Header background: {black;}",
+      "Header {background: black;}",
+      "{header background: black};",
+      "Header (background: black;)",
+    ],
+    correctAnswer: "Header {background: black;}",
+  },
+  //question 3
+  {
+    questionTitle: "How do you insert a comment in CSS?",
+    answers: [
+      "//  This way //",
+      "“ This way”",
+      "// This way ",
+      "*/ This way */ ",
+    ],
+    correctAnswer: "*/ This way */ ",
+  },
+  //question 4
+  {
+    questionTitle:
+      "In an HTML file where is the correct place to insert JavaScript tag?",
+    answers: [
+      "In the header section",
+      "At the start of the body section",
+      "At the bottom of the body section",
+      "In the footer section",
+    ],
+    correctAnswer: "At the bottom of the body section",
+  },
+  //question 5
+  {
+    questionTitle:
+      "Which of the following is a type of way to target HTML elements in CSS?",
+    answers: [
+      "Using a class selector",
+      "Using a variable name",
+      "Using a method",
+      "Using a list",
+    ],
+    correctAnswer: "Using a class selector",
+  },
+  //question 6
+  {
+    questionTitle: "How do write variable names in JavaScript",
+    answers: [
+      "All letters in capitals without spacing between words",
+      "All letters in lowercase with spacing between words",
+      "First letter in uppercase, with spaces and first letter of subsequent words uppercase",
+      "First letter in lowercase, no spaces and first letter of subsequent words uppercase",
+    ],
+    correctAnswer:
+      "First letter in lowercase, no spaces and first letter of subsequent words uppercase",
+  },
+  //question 7
+  {
+    questionTitle: "Which operator is used to assign a value to an operator?",
+    answers: ["+", "=", "*", "$"],
+    correctAnswer: "=",
+  },
+];
