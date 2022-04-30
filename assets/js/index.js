dd4560f0c3b34ea214690ff839b6b61b582df64c
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-04-30 23:49:51 +0100

    refactored code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
+// declare time in a variable
+let Time = 60;
+
+// targeting start button element
+const quizStartBnt = document.getElementById("quizStartBtn");
+
 // declaring question title, answers and correct answer as objects in an array
 const questions = [
   //question 1
@@ -77,8 +83,55 @@ const questions = [
   },
 ];
 
-// target the start button element
-const 
-//add an event lister button 
-//create to initiate function to start quiz with the click of a button 
+const onLoad = () => {
+  // initialise local storage
+  // check if high scores exists in LS
+  // if false then set high scores to empty array in LS
+  //create to initiate function to start quiz with the click of a button
+};
 
+//create a function for when the user clicks on an answer
+//create a function to valiate answers - render alert message
+//create a function for if the answer is wrong
+
+const startTimer = () => {
+  //target the main section
+  const mainSection = document.getElementById("main-tag");
+  // creating a div for timer section
+  const timerSection = document.createElement("section");
+  //using HTML to build the timer
+  timerSection.innerHTML = `<section>
+    <div id="timerStartBtn" class="timer-div">
+      Timer Countdown : <span id="timer-span">${60} seconds</span>
+    </div>
+  </section>`;
+  // append timer section to main section
+  mainSection.appendChild(timerSection);
+
+  // declare function to execute every 1 sec
+  const countdown = () => {
+    // decrement timer value
+    // if quizComplete is true then stop timer
+    // check if timer reaches 0
+    // if true render game over
+  };
+
+  // setInterval of 1000ms (1s)
+};
+
+const startQuiz = () => {
+  //remove the start section
+  const removeStartSection = () => {
+    //targets the main tag element
+    const startSection = document.getElementById("start-quiz-section");
+    //removes the start section
+    startSection.remove();
+  };
+  // call the remove start section function
+  removeStartSection();
+  //call the start timer function
+  startTimer();
+};
+
+// add start button click event listener
+quizStartBnt.addEventListener("click", startQuiz);
